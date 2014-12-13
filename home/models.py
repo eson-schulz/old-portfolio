b6e853af64d85c4dc7adfd5a970c96d4da5a0c80
eson-schulz <ethan@eschulz.com>
2014-12-12 19:36:39 -0600

    Added Job as a model to represent the list of jobs underneath my name on the home page

--- home/models.py
+++ home/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Job(models.Model):
+	name = models.CharField(max_length=128, unique=True)
+
+	def __unicode__(self):
+		return self.name
--- home/views.py
+++ home/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from home.models import Job
 
 def index(request):
 
-	return render(request, 'index.html')
+	job_list = Job.objects.order_by('id')
+	jobs = '- '.join([job.name for job in job_list])
+	context_dict = {'jobs': jobs}
+
+	return render(request, 'index.html', context_dict)
