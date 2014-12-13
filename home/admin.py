592077979201def792d9fb7dccff88f84774448a
eson-schulz <ethan@eschulz.com>
2014-12-13 16:12:57 -0600

    Updated the admin and views for the new location option

--- home/admin.py
+++ home/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 from home.models import Job, About
 
+class AboutAdmin(admin.ModelAdmin):
+
+	list_display = ('location', 'description')
+
 admin.site.register(Job)
-admin.site.register(About)
+admin.site.register(About, AboutAdmin)
--- home/views.py
+++ home/views.py
@@ -8,8 +8,12 @@ def index(request):
 	jobs = '- '.join([job.name for job in job_list])
 	context_dict['jobs'] = jobs
 
-	abouts = About.objects.order_by('id')
-	paragraphs = [about.description for about in abouts]
-	context_dict['about'] = paragraphs
+	# Gets the item that's the first paragraph, second paragraph, etc
+	name = ('first_paragraph', 'second_paragraph', 'footer_paragraph')
+	for x in range(0, 3):
+		try:
+			context_dict[name[x]] = About.objects.get(location=str(x + 1)).description
+		except(About.DoesNotExist):
+			context_dict[name[x]] = "Error: " + name[x] + " does not exist."
 
 	return render(request, 'index.html', context_dict)
