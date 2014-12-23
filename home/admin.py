64660e4ce5cf8b11f5f84710a9590b35bdba9cff
eson-schulz <ethan@eschulz.com>
2014-12-22 18:42:31 -0600

    Updated admin to display Projects nicer.

--- home/admin.py
+++ home/admin.py
@@ -5,6 +5,10 @@ class AboutAdmin(admin.ModelAdmin):
 
 	list_display = ('location', 'description')
 
+class ProjectAdmin(admin.ModelAdmin):
+
+	list_display = ('name', 'description', 'date')
+
 admin.site.register(Job)
 admin.site.register(About, AboutAdmin)
-admin.site.register(Project)
+admin.site.register(Project, ProjectAdmin)
