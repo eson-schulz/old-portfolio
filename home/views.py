ef1734ea3e30544c1c6276304e6dc93e1c1d7235
eson-schulz <ethan@eschulz.com>
2014-12-22 18:48:04 -0600

    Added projects sorted newest to oldest to be used in the template.

--- home/views.py
+++ home/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
-from home.models import Job, About  
+from home.models import Job, About, Project
 
 def index(request):
 	context_dict = {}
 
+	# Gets the list of names of jobs
 	job_list = Job.objects.order_by('id')
 	jobs = ' - '.join([job.name for job in job_list])
 	context_dict['jobs'] = jobs
@@ -15,5 +16,7 @@ def index(request):
 			context_dict[name[x]] = About.objects.get(location=str(x + 1)).description
 		except(About.DoesNotExist):
 			context_dict[name[x]] = "Error: " + name[x] + " does not exist."
+
+	context_dict['projects'] = Project.objects.order_by('-date')
 
 	return render(request, 'index.html', context_dict)
