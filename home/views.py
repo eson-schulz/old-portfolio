c08d272f2387099256c554533e0c635ddca9c5df
eson-schulz <ethan@eschulz.com>
2015-01-18 16:07:06 -0600

    Updated the views to insert the contact form into the template, and to send an email for the contact page.

--- home/views.py
+++ home/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from django.core.mail import send_mail
+from django.conf import settings
 from home.models import Job, About, Github, Project, Resume, Skill
+from home.forms import ContactForm
 import math
 
 def index(request):
@@ -37,5 +40,25 @@ def index(request):
 	# Gets the single Github Model
 	if(len(Github.objects.all()) > 0):
 		context_dict['github'] = Github.objects.all()[0]
+
+	# The Contact Form
+	if request.method == 'POST':
+		form = ContactForm(request.POST)
+
+		if form.is_valid():
+			context_dict['sent_message'] = "Thanks for the email!"
+
+			# Sends an email
+			subject = "Message from " + form.cleaned_data['name']
+			message = form.cleaned_data['message'] + "\n\n" + "-" * 60 + "\nThis was automatically sent from your website"
+			sender = form.cleaned_data['email']
+
+			recipients = [settings.USER_EMAIL]
+
+			send_mail(subject, message, sender, recipients)
+
+	else:
+		form = ContactForm()
+	context_dict['form'] = form
 
 	return render(request, 'index.html', context_dict)
